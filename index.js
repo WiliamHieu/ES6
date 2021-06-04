691883483921a6e2989d0fb013304aff81361da0
Wiliam <hieuvodinh98@gmail.com>
2021-06-04 17:09:50 +0700

    Spread

--- index.js
+++ index.js
@@ -16,12 +16,12 @@
 // const {street, city, country} = address;
 // const {street: st } = address;
 
-//  const first = [1, 2, 3];
-//  const second = [4, 5, 6]
+ const first = [1, 2, 3];
+ const second = [4, 5, 6]
 // Spread : create new array, spread all items of first and put it in new array
-//  const combined = first.concat(second);
-//  const combined = [...first, 'a', ...second]
+ const combined = first.concat(second);
+ const combined = [...first, 'a', ...second]
 
-// const first = {name: 'Mosh'};
-// const clone = {...rist};
+const first = {name: 'Mosh'};
+const clone = {...rist};
 
