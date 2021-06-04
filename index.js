302e55c8f39793dad57d356c09fc4ca8cf461692
Wiliam <hieuvodinh98@gmail.com>
2021-06-04 16:27:30 +0700

    callback func

--- index.js
+++ index.js
@@ -28,16 +28,31 @@
 //     return number * number;
 // }
 
-const job = [
-    {id:1, isActive: true},
-    {id:2, isActive: true},
-    {id:3, isActive: false}
-]
+// const job = [
+//     {id:1, isActive: true},
+//     {id:2, isActive: true},
+//     {id:3, isActive: false}
+// ]
 
-const activejob = job.filter(function(job){ return job.isActive; })
-const activejob = job.filter(job => job.isActive)
+// const activejob = job.filter(function(job){ return job.isActive; })
+// const activejob = job.filter(job => job.isActive)
 
 
 // const square = number => number * number;
 // console.log(square(5));\
+
+
+// //Call back is stand-online object
+// const person = {
+//     talk() {
+//         //No close brace this ketword, this is not reset instead 
+//         //it in herites context that you defined (slef = this)
+//         var self = this;
+//         setTimeout(() => {
+//             console.log('self', self)
+//         }, 1000);
+//     }
+// }
+
+// person.talk();
 
