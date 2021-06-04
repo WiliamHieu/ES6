bed63e387ae02bab9061bae675bf82fc603ecfa9
Wiliam <hieuvodinh98@gmail.com>
2021-06-04 15:30:09 +0700

    ES6

---
+++ index.js
@@ -0,0 +1,32 @@
+// var -> function (dont need)
+// let ->block
+// const -> block
+
+// function sayHello() {
+//     for (let i=0; i < 5; i++){
+//         console.log(i)
+//     }
+//     console.log(i)
+// }
+// sayHello();
+
+// const person = {
+//     name: 'Mosh',
+//     talk() {
+//         console.log(this)
+//     }
+// }
+
+// person.talk();
+
+// const talk = person.talk.bind(person);
+// talk();
+
+// Arow function
+
+// const square = function() {
+//     return number * number;
+// }
+
+const square = number => number * number;
+console.log(square(5));
