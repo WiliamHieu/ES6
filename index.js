ed7baf46066c342330cc2acf5d8b6f97ad934022
Wiliam <hieuvodinh98@gmail.com>
2021-06-04 17:46:19 +0700

    Class concept

--- index.js
+++ index.js
@@ -1,84 +1,28 @@
-// var -> function (dont need)\
-// let ->block
-// const -> block
-
-// function sayHello() {
-//     for (let i=0; i < 5; i++){
-//         console.log(i)
+//First word should be uppercase
+// CLASS
+// class Person {
+//     constructor(name) {
+//         this.name = name;
 //     }
-//     console.log(i)
-// }
-// sayHello();
-
-// const person = {
-//     name: 'Mosh',
-//     talk() {
-//         console.log(this)
+//     walk() {
+//         console.log('Walk');
 //     }
 // }
 
-// person.talk();
+// const person = new Person()
+// person.walk();
 
-// const talk = person.talk.bind(person);
-// talk();
+// class Teacher extends Person{
+//     constructor(name, degree) {
+//         super(name);
+//         this.degree = degree;
+//     }
 
-// Arow function
-
-// const square = function() {
-//     return number * number;
+//     teach () {
+//         console.log("teach");
+//     }
 // }
 
-// const job = [
-//     {id:1, isActive: true},
-//     {id:2, isActive: true},
-//     {id:3, isActive: false}
-// ]
-
-// const activejob = job.filter(function(job){ return job.isActive; })
-// const activejob = job.filter(job => job.isActive)
-
-
-// const square = number => number * number;
-// console.log(square(5));\
-
-
-//Call back is stand-online object
-const person = {
-    talk() {
-        //No close brace this ketword, this is not reset instead 
-        //it in herites context that you defined (slef = this)
-        var self = this;
-        setTimeout(() => {
-            console.log('self', self)
-        }, 1000);
-    }
-}
-
-// person.talk();
-// const color =['red','green','blue'];
-// // template literal in es6
-// const items = color.map(color => '<li>${color}</li>');
-// console.log(items)
-
-// const address = {
-//     street: '',
-//     city: '',
-//     country: ''
-// };
-
-// const street = street.address;
-// const city = city.address;
-// const country = country.address;
-//// Object restructure
-// const {street, city, country} = address;
-// const {street: st } = address;
-
- const first = [1, 2, 3];
- const second = [4, 5, 6]
-// Spread : create new array, spread all items of first and put it in new array
- const combined = first.concat(second);
- const combined = [...first, 'a', ...second]
-
-const first = {name: 'Mosh'};
-const clone = {...rist};
+// const teacher = new Teacher();
+// teacher.
 
